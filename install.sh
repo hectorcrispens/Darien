3108c075be1e78b9b77ca044b04e54e2c9a4a9fd
hector crispens <hector.or.cr@gmail.com>
2020-04-29 15:09:44 -0300

    upload version 3.1

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+IRed='\033[0;91m'         # Red
+Cyan='\033[0;36m'         # Cyan
+Color_Off='\033[0m'       # Text Reset
 echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "::                                                                       ::"
-echo "::                                     DARIEN                            ::"
+echo -e "::                                     ${Cyan}DARIEN Versión 3.1 ${Color_Off}               ::"
 echo "::                                    Simple command for developers      ::"
 echo "::                                                                       ::"
 echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
@@ -22,11 +25,28 @@ echo ""
 read -p "¿Desea instalar docker y docker compose? y/n: " choice
 echo ""
 if [ "$choice" = "y" ]; then
+apt-get remove docker docker-engine docker.io containerd runc
+sleep 5
+apt-get update
+sleep 5
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+sleep 5
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sleep 5
+sudo apt-key fingerprint 0EBFCD88
+sleep 2
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
+sleep 5
 apt-get update
+sleep 5
 apt-get install docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
@@ -34,44 +54,58 @@ ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 fi
 
 echo ""
-echo -ne "\r--[ check files ]---------------------------------------------------------"
+echo -ne "\r${Cyan}--[ ${IRed}check files ${Cyan}]---------------------------------------------------------${Color_Off}"
 sleep 2
+if [ -e dependencies.tar ]; then
 
-if [ -e instalacion/messages.dar ] && [ -e instalacion/functions.dar ] && [ -e instalacion/menu.dar ]; then
-    echo -ne "\r------------------------[ build command ]---------------------------------"
+echo -ne "\r${Cyan}------------[ ${IRed}decompress ${Cyan}]------------------------------------------------${Color_Off}"
 sleep 2
-    touch darien
-    echo "#/bin/bash" >> darien
-    echo "" >> darien
+tar -xvf dependencies.tar > /dev/null 2>&1
+echo -ne "\r${Cyan}------------------------[ ${IRed}build command ${Cyan}]---------------------------------${Color_Off}"
+sleep 2
+    touch darien.sh
+    echo "#!/bin/bash" >> darien.sh
+    echo "" >> darien.sh
     #input="instalacion/messages.dar"
 	
 	#	while IFS= read -e line
 	#		do
 	#			echo $line >> darien
 	#	done < "$input"
-    cat instalacion/messages.sh >> darien
-    cat instalacion/functions.sh >> darien
-    cat instalacion/menu.sh >> darien
+    cat source/colors.sh >> darien.sh
+    cat source/notice.sh >> darien.sh
+    cat source/extras.functions.sh >> darien.sh
+    cat source/estructuras.sh >> darien.sh
+    cat source/compose.functions.sh >> darien.sh
+    cat source/dockerfiles.functions.sh >> darien.sh
+    cat source/level-1.functions.sh >> darien.sh
+    cat source/menu.sh >> darien.sh
+
+
+
        
-echo -ne "\r--------------------------------------------[ install command ]-----------"
+echo -ne "\r${Cyan}---------------------------------[  ${IRed}install command ${Cyan}]---------------------${Color_Off}"
 sleep 2
-chmod 777 darien
-cp darien /usr/bin/darien
-echo -ne "\r-----------------------------------------------------------------[ done ]-"
+chmod 777 darien.sh
+cp darien.sh /usr/bin/darien
+echo -ne "\r${Cyan}--------------------------------------------[  ${IRed}remove post install ${Cyan}]------${Color_Off}"
+sleep 2
+rm -r source
+echo -ne "\r${Cyan}-----------------------------------------------------------------[ ${IRed}done ${Cyan}]-${Color_Off}"
 sleep 2
 echo ""
 else
 echo ""
-echo -ne "\r---------[ downloading ]--------------------------------------------------"
+echo -ne "\r${Cyan}---------[  ${IRed}downloading ${Cyan}]-------------------------------------------------${Color_Off}"
 sleep 2
 wget "https://sourceforge.net/projects/darien/files/darien.sh/download" > /dev/null 2>&1
-echo -ne "\r-----------------------[ change permission ]------------------------------"
+echo -ne "\r${Cyan}-----------------------[ ${Ired}change permission ${Cyan}]------------------------------${Color_Off}"
 sleep 2
 chmod 777 download
-echo -ne "\r--------------------------------------------[ install command ]-----------"
+echo -ne "\r${Cyan}--------------------------------------------[  ${IRed}install command ${Cyan}]----------${Color_Off}"
 sleep 2
 mv download /usr/bin/darien
-echo -ne "\r-----------------------------------------------------------------[ done ]-"
+echo -ne "\r${Cyan}----------------------------------------------------------------[  ${IRed}done ${Cyan}]-${Color_Off}"
 sleep 2
 echo ""
 fi
