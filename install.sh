a0f3b47174b26811b6101ea0ecd401ff87650acc
hectorcrispens <hector.or.cr@gmail.com>
2020-05-08 11:43:44 -0300

    update version 4.0 untested

--- install.sh
+++ install.sh
@@ -4,10 +4,8 @@ Cyan='\033[0;36m'         # Cyan
 Color_Off='\033[0m'       # Text Reset
 echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
-echo "::                                                                       ::"
-echo -e "::                                     ${Cyan}DARIEN Versión 3.1 ${Color_Off}               ::"
+echo -e "::                                     ${Cyan}DARIEN Versión 4.0 ${Color_Off}               ::"
 echo "::                                    Simple command for developers      ::"
-echo "::                                                                       ::"
 echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 
@@ -22,76 +20,49 @@ echo "                                              esperemos que sea de utilida
 echo "Antes de comenzar, Darien funciona sobre docker y docker-compose, si ambas "
 echo "herramientas no estan instaladas, este instalador lo puede hacer por usted "
 echo ""
-read -p "¿Desea instalar docker y docker compose? y/n: " choice
-echo ""
-if [ "$choice" = "y" ]; then
-apt-get remove docker docker-engine docker.io containerd runc
-sleep 5
-apt-get update
-sleep 5
-apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-sleep 5
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sleep 5
-sudo apt-key fingerprint 0EBFCD88
-sleep 2
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-sleep 5
-apt-get update
-sleep 5
-apt-get install docker-ce docker-ce-cli containerd.io
-curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-fi
-
 echo ""
 echo -ne "\r${Cyan}--[ ${IRed}check files ${Cyan}]---------------------------------------------------------${Color_Off}"
 sleep 2
-if [ -e dependencies.tar ]; then
+if [ -e layers.tar ]; then
 
 echo -ne "\r${Cyan}------------[ ${IRed}decompress ${Cyan}]------------------------------------------------${Color_Off}"
 sleep 2
-tar -xvf dependencies.tar > /dev/null 2>&1
+tar -xvf layers.tar > /dev/null 2>&1
 echo -ne "\r${Cyan}------------------------[ ${IRed}build command ${Cyan}]---------------------------------${Color_Off}"
 sleep 2
-    touch darien.sh
-    echo "#!/bin/bash" >> darien.sh
-    echo "" >> darien.sh
+    namefile="darien.sh"
+    namefileoutput="darien"
+    touch $namefile
+    echo "#!/bin/bash" >> $namefile
+    echo "" >> $namefile
     #input="instalacion/messages.dar"
 	
 	#	while IFS= read -e line
 	#		do
 	#			echo $line >> darien
 	#	done < "$input"
-    cat source/colors.sh >> darien.sh
-    cat source/notice.sh >> darien.sh
-    cat source/extras.functions.sh >> darien.sh
-    cat source/estructuras.sh >> darien.sh
-    cat source/compose.functions.sh >> darien.sh
-    cat source/dockerfiles.functions.sh >> darien.sh
-    cat source/level-1.functions.sh >> darien.sh
-    cat source/menu.sh >> darien.sh
+    cat layers/colors.sh >> $namefile
+    cat layers/layer5-informations.sh >> $namefile
+    cat layers/layer4-workers.sh >> $namefile
+    cat layers/layer3-injectors.sh >> $namefile
+    cat layers/layer2-customers.sh >> $namefile
+    cat layers/layer2-builders.sh >> $namefile
+    cat layers/layer2-tasker.sh >> $namefile
+    cat layers/layer1-definitions.sh >> $namefile
+    cat layers/menu.sh >> $namefile
+   
 
 
 
        
 echo -ne "\r${Cyan}---------------------------------[  ${IRed}install command ${Cyan}]---------------------${Color_Off}"
 sleep 2
-chmod 777 darien.sh
-cp darien.sh /usr/bin/darien
+chmod 777 $namefile
+cp $namefile /usr/bin/$namefileoutput
 echo -ne "\r${Cyan}--------------------------------------------[  ${IRed}remove post install ${Cyan}]------${Color_Off}"
 sleep 2
-rm -r source
-rm darien.sh
+rm -r layers
+rm $namefile
 echo -ne "\r${Cyan}-----------------------------------------------------------------[ ${IRed}done ${Cyan}]-${Color_Off}"
 sleep 2
 echo ""
@@ -105,7 +76,7 @@ sleep 2
 chmod 777 download
 echo -ne "\r${Cyan}--------------------------------------------[  ${IRed}install command ${Cyan}]----------${Color_Off}"
 sleep 2
-mv download /usr/bin/darien
+mv download /usr/bin/$namefileoutput
 echo -ne "\r${Cyan}----------------------------------------------------------------[  ${IRed}done ${Cyan}]-${Color_Off}"
 sleep 2
 echo ""
